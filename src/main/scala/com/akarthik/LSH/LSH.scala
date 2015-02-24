acdc7628ceeefd36660031e1f32f38846abfd242
bel <bel@jusbrasil.com.br>
2015-02-23 21:29:37 -0300

    [LSH] Commented the code

--- src/main/scala/com/akarthik/LSH/LSH.scala
+++ src/main/scala/com/akarthik/LSH/LSH.scala
@@ -24,18 +24,19 @@ class LSH(shingleLength: Int = 3,
   processedDocuments: IndexedSeq[(String, Int)],
   threshold: Double=0.8) {
 
-  val randomHashFunctions = randomLinearHashFunction(minHashLength);
+  val randomHashFunctions = randomLinearHashFunction(minHashLength); // IndexSeq[(int, int)]
 
   val documentShingles: Map[Int, Set[String]] = processedDocuments.map { document =>
     val shingles = document._1.toList.sliding(shingleLength)
       .map(_.mkString).toSet
     (document._2, shingles)
-  }.toMap
+  }.toMap // documentShingles is a Map -> key == document index e value == set of shingles of the document
 
   val shingleVocab = documentShingles.values.flatten.toSet.toIndexedSeq.zipWithIndex.toMap
 
   var mBands: IndexedSeq[Band] = null;
 
+  // Create an IndexSeq[(int, int)] with random values from 0 to 1000
   private def randomLinearHashFunction(n: Int) = {
     val slope = scala.util.Random.shuffle(0 to 1000);
     val const = scala.util.Random.shuffle(0 to 1000);
@@ -46,62 +47,66 @@ class LSH(shingleLength: Int = 3,
   def findCandidates(shingles: Set[String]) = {
     val minHash = getMinHash(shingles);
 
-    val subArrays = partitionArray(minHash).zipWithIndex;
+    val subArrays = partitionArray(minHash).zipWithIndex; // IndexSeq[(Array[Double], Int)] ->
+                                                          // Array[Double] => part of the minHash
+                                                          // Int => index of the part
 
     val candidates = subArrays.flatMap { subArray =>
       val index = subArray._2;
-      val hashedBucket = mBands(index).getCollisionObjects(subArray._1)
+      val hashedBucket = mBands(index).getCollisionObjects(subArray._1) // encontra a lista com os indices dos documentos que colidiram para essa band
       hashedBucket
-    }.flatten.toSet
+    }.flatten.toSet // cada band retornara uma List com os indices dos documentos que colidiram, entao candites ate esse ponto eh IndexSeq[List[Int]]
+    // transforma em mutavel e depois em um SET (pra remover os indices repetidos acredito eu), entao no final tem-se canidates = Set[Int] com os indices que colidiram
 
     candidates
   }
 
-/** Returns documents that have Jaccard Similarity greater than threshold Assumes that a documents
-   have already been hashed 
+  /** Returns documents that have Jaccard Similarity greater than threshold Assumes that a documents
+   have already been hashed
   @tparam : document . The document for which similar documents have to be identified
-**/
+    **/
 
   def findSimilar(document: String) = {
     val shingles = document.toList.sliding(shingleLength)
-    .map(_.mkString)
-    .map(shingle => shingle.toLowerCase)
-    .toSet;
+      .map(_.mkString)
+      .map(shingle => shingle.toLowerCase)
+      .toSet;
 
     val candidates = findCandidates(shingles);
-    candidates.filter(candidate => JaccardSimilarity(shingles, documentShingles(candidate.toInt)) > threshold)
+    candidates.filter(candidate => JaccardSimilarity(shingles, documentShingles(candidate)) > threshold) // vai comparar o shingle do documento 'atual' (o qual busca-se a similaridade),
+    // com o shingle de cada documento que foi dito como candidato. Aqueles que forem maior que o theshold sao considerados documentos similares
   }
 
-/** Returns the Min Hash of a document 
-  *@tparam : The shingle representation for that document
-**/
+  /** Returns the Min Hash of a document
+    *@tparam : The shingle representation for that document
+    **/
 
   def getMinHash(shingles: Set[String]) = {
 
-    val minHash = Array.fill[Double](minHashLength)(Double.PositiveInfinity);
+    val minHash = Array.fill[Double](minHashLength)(Double.PositiveInfinity); // inicializa um Array de doubles de tamanho 'minHashLength' com Infinitys
 
-    shingles.filter(x => shingleVocab.contains(x))
-      .foreach { shingle =>
-        val shingleIndex = shingleVocab(shingle);
-        var hashIndex = 0;
-        randomHashFunctions.foreach { function =>
-          val permutedIndex = (function._1 * shingleIndex + function._2) % shingleVocab.size
+    shingles.filter(x => shingleVocab.contains(x)) // filtra apenas os shingles que estao no 'shingleVocab'
+      .foreach { shingle =>                        // pra cada shingle
+      val shingleIndex = shingleVocab(shingle);    // pega o indice dele no 'shingleVocab'
+    var hashIndex = 0;
+      randomHashFunctions.foreach { function =>  // pra cada hash function
+        val permutedIndex = (function._1 * shingleIndex + function._2) % shingleVocab.size // aplica o Hash => (valor1_hash + indice + valor2_hash) / tamanho do 'shingleVocab'
 
-          if (minHash(hashIndex) > permutedIndex)
-            minHash(hashIndex) = permutedIndex
+        if (minHash(hashIndex) > permutedIndex) // se o valor para o indice atual for maior do que o 'permutedIndex' esse valor eh substituido
+          minHash(hashIndex) = permutedIndex    // com isso o valores de 'minHash' que eram 'Infinity' vao sendo atualizados
 
-          hashIndex += 1;
-        }
-
+        hashIndex += 1; // o indice atual da 'minHash' eh atualizado
       }
+
+    }
 
     minHash
 
   }
 
-/** Partition the min-hash into  numberBands bands  
+  /** Partition the min-hash into  numberBands bands
   @tparam : The shingle represenatation of the document
-**/
+    **/
 
   def partitionArray(minHash: Array[Double]): IndexedSeq[Array[Double]] = {
 
@@ -115,24 +120,28 @@ class LSH(shingleLength: Int = 3,
       val start = bandIndex * elementsPerBand
       val end = start + elementsPerBand;
       minHash.slice(start, end);
-    }
+    } // cria um IndexedSeq[Array[Double]] onde cada elemento eh um pedaco do minHash, o tamanho desse pedaco eh definido por 'elementsPerBand'
+    // o tamanho do 'IndexedSeq' sera igual ao numero de bands
   }
 
- /** Creates a locality sensitive hash for the all the processed documents **/
+  /** Creates a locality sensitive hash for the all the processed documents **/
 
   def createHash() = {
 
-    val minHashCollection = documentShingles.mapValues(shingleSet => getMinHash(shingleSet))
+    val minHashCollection = documentShingles.mapValues(shingleSet => getMinHash(shingleSet)) // um Map[Int, Array[Double]], onde chave == indice do documento e valor == minHash
     val bands =
 
       (0 to numberBands - 1).map { bandIndex =>
         val elementsPerBand = (1.0 * minHashLength / numberBands).ceil.toInt
         val start = bandIndex * elementsPerBand
-        val end = if (bandIndex == numberBands - 1) minHashLength else start + elementsPerBand;
-        val subArray = minHashCollection.map(document => (document._1, document._2.slice(start, end)))
-        val band = new Band()
-        subArray.foreach(array => band.hash(array))
-        band
+        val end = if (bandIndex == numberBands - 1) minHashLength else start + elementsPerBand; // se o 'bandIndex' for igual ao limite o 'end' recebe o tamanho do array
+      val subArray = minHashCollection.map(document => (document._1, document._2.slice(start, end))) // Map[Int, Array[Double]] ->
+                                                                                                    // Int => indice do documento,
+                                                                                                    // Array[Double] => pedaco do min hash
+                                                                                                    // cada linha desse Map representa um documento
+      val band = new Band()
+        subArray.foreach(array => band.hash(array)) //mapeia o pedaco do min hash pro bucket (ver comentarios do metodo 'hash')
+        band // cada band representa um pedaco do minHash, sendo que cada minHash eh mapeado pra um bucket geral
       }
 
     mBands = bands
@@ -145,22 +154,22 @@ class LSH(shingleLength: Int = 3,
 class Band() {
   import scala.collection.mutable.ArrayBuffer
 
-  val buckets = scala.collection.mutable.Map[List[Double], ArrayBuffer[Int]]()
+  val buckets = scala.collection.mutable.Map[List[Double], ArrayBuffer[Int]]() // chave == pedaco do minHash e valor == indice do documento
 
-/** Hashes the sub- array into buckets **/
+  /** Hashes the sub- array into buckets **/
   def hash(subArray: (Int, Array[Double])) {
-    buckets.get(subArray._2.toList) match {
-      case Some(value: ArrayBuffer[Int]) => value += subArray._1;
-      case None => buckets(subArray._2.toList) = ArrayBuffer(subArray._1)
+    buckets.get(subArray._2.toList) match { // pega o bucket correspondente ao pedaco do minHash
+      case Some(value: ArrayBuffer[Int]) => value += subArray._1; //caso esse bucket exista, o indice do documento eh adicionado no array referente a tal bucket
+      case None => buckets(subArray._2.toList) = ArrayBuffer(subArray._1) //caso nao exista esse bucket ainda ele eh criado e o indice do documento eh adicionado ao seu array
     }
 
   }
 
-/** Returns the documents that collide to the same bucket **/
+  /** Returns the documents that collide to the same bucket **/
   def getCollisionObjects(subArray: Array[Double]): Option[List[Int]] = {
     buckets.get(subArray.toList) match {
-      case Some(value: ArrayBuffer[Int]) => Some(value.toList);
-      case None => buckets(subArray.toList) = ArrayBuffer(-1); None
+      case Some(value: ArrayBuffer[Int]) => Some(value.toList); //caso o bucket correspondente ao 'subArray' exista, a lista dos indices dos documentos eh retornada
+      case None => buckets(subArray.toList) = ArrayBuffer(-1); None //caso contrario, cria-se tal bucket e adiciona -1 ao seu array (Pq isso??)
     }
   }
 
@@ -176,4 +185,3 @@ object JaccardSimilarity {
     return (intersection * 1.0) / union
   }
 }
-
